d3213e78d076ab0bf63fae05881da50f0485ad5e
xdandsl <15733221796@163.com>
2018-12-04 18:55:26 +0800

    注册路由和登陆路由完成

--- router/index.js
+++ router/index.js
@@ -2,12 +2,92 @@
  * router管理路由器模块
  */
 
-const express = require('express') ;
+const express = require('express');
+const Users = require('../module/index');
+//引入此模块对密码进行加密
+const md5 = require('blueimp-md5');
+
 const router = new express.Router();
 
+//解析请求体数据
+router.use(express.urlencoded({extended: true}));
 
-router.get('/' , (req,res) => {
-  res.send('这是服务器发来的响应')
+router.get('/', (req, res) => {
+  res.send('这是服务器返回的响应222');
+})
+
+//注册逻辑路由：
+router.post('/register', async (req, res) => {
+  //获取用户提交请求参数信息
+  const {username, password, type} = req.body;
+  console.log(username, password, type);
+
+  try {
+    //去数据库查找当前用户是否存在
+    const user = await Users.findOne({username});
+    if (user) {
+      //用户名被注册了
+      res.json({
+        code: 1,
+        msg: '此用户已存在'
+      })
+    } else {
+      //用户可以注册
+      //保存在数据库中
+      const user = await Users.create({username, password, type});
+      //返回成功的响应
+      res.json({
+        code: 0,
+        data: {
+          username: user.username,
+          _id: user.id,
+          type: user.type
+        }
+      })
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({
+      code: 2,
+      msg: '网络不稳定，请刷新试试~'
+    })
+  }
 });
+
+//登陆逻辑的路由：
+router.post('/login', async (req, res) => {
+  //获取用户提交请求参数信息
+  const {username, password} = req.body;
+  console.log(username, password);
+
+  try {
+    //去数据库查找当前用户是否存在
+    const user = await Users.findOne({username, password: md5(password)});
+    if (user) {
+      //用户可登录，登录成功
+      res.json({
+        code: 0,
+        data: {
+          _id: user.id,
+          type: user.type,
+          username: user.username
+        }
+      })
+    } else {
+      //用户名或密码错误
+      //返回失败的响应
+      res.json({
+        code: 1,
+        msg: '用户名或密码错误'
+      })
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({
+      code: 2,
+      msg: '网络不稳定，请刷新试试~'
+    })
+  }
+})
 
 module.exports = router;
